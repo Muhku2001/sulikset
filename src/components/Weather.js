1f596470848baf5a49fddf561e32b47f4694d014
Muhku2001 <69837166+Muhku2001@users.noreply.github.com>
2020-09-03 14:53:32 +0300

    3.9.2020

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,9 +3,22 @@ import { VictoryChart, VictoryLine } from 'victory'
 
 
 function Weather() {
-
+    const data = [
+        {quarter: 1, earnings: 13000},
+        {quarter: 2, earnings: 16500},
+        {quarter: 3, earnings: 14250},
+        {quarter: 4, earnings: 19000}
+      ];
 
     return (
+    <div>    
+        <VictoryBar
+        data={data}
+        // data accessor for x values
+        x="quarter"
+        // data accessor for y values
+        y="earnings"
+      />
         <VictoryChart
             domainPadding={{x:30, y:10}}
                 width={10000}
@@ -31,6 +44,7 @@ function Weather() {
         
         />
         </VictoryChart>
+        </div>
     )
 }
 
