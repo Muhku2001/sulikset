4dfd39d357cc9dac55cc67dde6b91442406bcf47
Muhku2001 <69837166+Muhku2001@users.noreply.github.com>
2020-09-10 11:36:37 +0300

    10.9.2020

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { VictoryChart, VictoryLine, VictoryStack, VictoryHistogram } from 'victory'
+import { VictoryChart, VictoryLine, VictoryStack, VictoryArea, VictoryScatter, VictoryTooltip, VictoryVoronoiContainer } from 'victory'
 
 
 function Weather() {
@@ -13,48 +13,91 @@ function Weather() {
     return (
     <div>    
         <VictoryChart
+
                 domainPadding={{x:30, y:10}}
                 width={1000}
-                height={250}>
+                height={250}
+                containerComponent={<VictoryVoronoiContainer/>}
+                >
                 <VictoryLine
             data={[
-                {experiment: "1.1.", actual: -10},
-                {experiment: "2.1.", actual: -5},
-                {experiment: "3.1.", actual: -0},
-                {experiment: "1.1.", actual: +5},
-                {experiment: "2.1.", actual: +10}
+                {experiment: "10.9.", actual: -10},
+                {experiment: "12.9.", actual: -5},
+                {experiment: "13.9.", actual: -0},
+                {experiment: "15.9.", actual: +10}
             ]}
             style={{data:
-                    {stroke: "green", strokeWidth: 1}
+                    {stroke: "tomato", strokeWidth: 5}
             }}
             x="experiment"
             y="actual"
         
         />
+         <VictoryScatter
+    style={{
+      data: {fill: "blue"}, labels: {fill: "blue"}
+    }}
+    size={(datum, active) => active ? 5 : 3}
+    labels={({ datum }) => datum.y}
+    labelComponent={<VictoryTooltip/>}
+    data={[
+      {x: 1, y: -10},
+      {x: 2, y: -5},
+      {x: 3, y: 0},
+      {x: 4, y: +10}
+    ]}
+  />
         </VictoryChart>
         
         <VictoryChart
-
-        domainPadding={{x:30, y:10}}
-                width={1000}
-                height={250}>
-                <VictoryLine
-            data={[
-                {experiment: "1.1.", actual: 0, label: '0%'},
-                {experiment: "2.1.", actual: 20, label: '20%'},
-                {experiment: "3.1.", actual: 40, label: '40%'},
-                {experiment: "4.1.", actual: 20, label: '20%'},
-                {experiment: "5.1.", actual: 80, label: '80%'}
-            ]}
-            style={{data:
-                    {stroke: "green", strokeWidth: 1}
-            }}
-            x="experiment"
-            y="actual"
-            />
-
-    
-        </VictoryChart>
+    domainPadding={{x:30, y:10}}
+            width={1000}
+            height={250}
+    events={[{
+    childName: ["area-1", "area-2"],
+    target: "data",
+    eventHandlers: {
+      onClick: () => {
+        return [{
+          childName: "area-4",
+          mutation: (props) => {
+            const fill = props.style.fill;
+            return fill === "tsmoke" ? null : {style: {fill: "tsmoke"}};
+          }
+        }];
+      }
+    }
+  }]}
+>
+  <VictoryStack>
+    <VictoryArea name="area-1"
+      data={[
+        {x: "10.9", y: 2}, {x: "11.9", y: 3}, {x: "12.9", y: 5}, {x: "13.9", y: 4}
+      ]}
+      data={[
+        {experiment: "10.9.", actual: 0, label: '0%'},
+        {experiment: "11.9.", actual: 20, label: '20%'},
+        {experiment: "12.9.", actual: 60, label: '60%'},
+        {experiment: "13.9.", actual: 0, label: '0%'}
+    ]}
+    x="experiment"
+    y="actual"
+    />
+    <VictoryArea name="area-2"
+      data={[
+        {x: "14.9", y: 1}, {x: "15.9", y: 4}, {x: "16.9", y: 5}, {x: "17.9", y: 7}
+      ]}
+      data={[
+        {experiment: "14.9.", actual: 0, label: '0%'},
+        {experiment: "15.9.", actual: 20, label: '20%'},
+        {experiment: "16.9.", actual: 40, label: '40%'},
+        {experiment: "17.9.", actual: 0, label: '0%'}
+    ]}
+    x="experiment"
+    y="actual"
+    />
+  </VictoryStack>
+</VictoryChart>
     
         
         </div>
