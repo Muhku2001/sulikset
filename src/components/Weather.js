0f1bba4a66f2cf7fe6698ec849e52473d49ea0f5
Muhku2001 <69837166+Muhku2001@users.noreply.github.com>
2020-09-22 10:23:36 +0300

    22.9.2020

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,32 +3,45 @@ import { VictoryChart, VictoryLine, VictoryStack, VictoryArea, VictoryScatter, V
 
 
 function Weather() {
-  const data = [
-    { quarter: 1, earnings: 13000 },
-    { quarter: 2, earnings: 16500 },
-    { quarter: 3, earnings: 14250 },
-    { quarter: 4, earnings: 19000 }
-  ];
-
   const today = new Date();
   const date = today.getDate() + "." + parseInt(today.getMonth() + 1) + "." + today.getFullYear();
 
   const initWeather = [];
   const [weather, setWeather] = useState(initWeather);
 
-  fetch('https://funcvariaiot.azurewebsites.net/api/HttpTriggerGetIotData?code=qO5qkShg0osHqY0BB2nfXI/anPgQ/K/3mIF7VTCFfaTdrvo6wl6DKw==')
+  fetch('https://funcvariaiot.azurewebsites.net/api/HttpTriggerGetIotData?code=qO5qkShg0osHqY0BB2nfXI/anPgQ/K/3mIF7VTCFfaTdrvo6wl6DKw==&amount=50')
     .then(response => response.json())
     .then(json => setWeather([...json]));
 
+  let humtempkey = 1;
+  let chartTempDots = [];
+  let chartDataTemp = [];
+  let chartDataHum = [];
 
+
+ 
   const rows = () => weather.slice(0, 24).reverse().map(temphum => {
     const measurementDate = temphum.PublishedAt.split('T')[0].split('-')[2] +'.'+ temphum.PublishedAt.split('T')[0].split('-')[1] +'.'+temphum.PublishedAt.split('T')[0].split('-')[0]
-    const measurementTime = temphum.PublishedAt.split('T')[1].split(':')[0] +':'+ temphum.PublishedAt.split('T')[1].split(':')[1]  
-    return <div><b>Pvm: </b>{measurementDate}, <b>klo</b> {measurementTime}--- <b>Ilmankosteus: </b>{temphum.Hum.split('.')[0]}%---<b>Lämpötila: </b>{temphum.Temp.split('.')[0]}°C</div>
+    const measurementTime = temphum.PublishedAt.split('T')[1].split(':')[0] +':'+ temphum.PublishedAt.split('T')[1].split(':')[1] 
+    chartDataHum.push({experiment: String( measurementTime), actual: parseInt(temphum.Hum), label: String(temphum.Hum.split(".")[0]+"%")});
+    chartDataTemp.push({experiment: String( measurementTime), actual: parseInt(temphum.Temp)  })
+    chartTempDots.push({x: parseInt(humtempkey++), y: parseInt(temphum.Temp) })
+    return <div key={humtempkey}><b>Pvm: </b>{measurementDate}, <b>klo</b> {measurementTime}--- <b>Ilmankosteus: </b>{temphum.Hum.split('.')[0]}%---<b>Lämpötila: </b>{temphum.Temp.split('.')[0]}°C</div>
                                               
   })
-
-
+  const showChartHum = chartDataHum;
+ 
+const showChartTemp = chartDataTemp;
+/*
+const showTempDots = [
+  { x: 1, y: -10 },
+  { x: 2, y: -5 },
+  { x: 3, y: 0 },
+  { x: 4, y: +10 }
+]
+*/
+console.log(chartTempDots);
+const showTempDots = chartTempDots;
   return (
     <div align="center">
 
@@ -55,12 +68,7 @@ function Weather() {
         containerComponent={<VictoryVoronoiContainer />}
       >
         <VictoryLine
-          data={[
-            { experiment: "10.9.", actual: -10 },
-            { experiment: "12.9.", actual: -5 },
-            { experiment: "13.9.", actual: -0 },
-            { experiment: "15.9.", actual: +10 }
-          ]}
+          data={showChartTemp}
           style={{
             data:
               { stroke: "tomato", strokeWidth: 5 }
@@ -73,24 +81,19 @@ function Weather() {
           style={{
             data: { fill: "blue" }, labels: { fill: "blue" }
           }}
-          size={(datum, active) => active ? 5 : 3}
+          size={(active) => active ? 5 : 3}
           labels={({ datum }) => datum.y}
           labelComponent={<VictoryTooltip />}
-          data={[
-            { x: 1, y: -10 },
-            { x: 2, y: -5 },
-            { x: 3, y: 0 },
-            { x: 4, y: +10 }
-          ]}
+          data={showTempDots}
         />
       </VictoryChart>
 
       <VictoryChart
-        domainPadding={{ x: 30, y: 10 }}
-        width={1000}
-        height={250}
+        domainPadding={{ x: 1, y: 10 }}
+        width={1300}
+        height={350}
         events={[{
-          childName: ["area-1", "area-2"],
+          childName: ["area-1"],
           target: "data",
           eventHandlers: {
             onClick: () => {
@@ -107,28 +110,7 @@ function Weather() {
       >
         <VictoryStack>
           <VictoryArea name="area-1"
-            data={[
-              { x: "10.9", y: 2 }, { x: "11.9", y: 3 }, { x: "12.9", y: 5 }, { x: "13.9", y: 4 }
-            ]}
-            data={[
-              { experiment: "10.9.", actual: 0, label: '0%' },
-              { experiment: "11.9.", actual: 20, label: '20%' },
-              { experiment: "12.9.", actual: 60, label: '60%' },
-              { experiment: "13.9.", actual: 0, label: '0%' }
-            ]}
-            x="experiment"
-            y="actual"
-          />
-          <VictoryArea name="area-2"
-            data={[
-              { x: "14.9", y: 1 }, { x: "15.9", y: 4 }, { x: "16.9", y: 5 }, { x: "17.9", y: 7 }
-            ]}
-            data={[
-              { experiment: "14.9.", actual: 0, label: '0%' },
-              { experiment: "15.9.", actual: 20, label: '20%' },
-              { experiment: "16.9.", actual: 40, label: '40%' },
-              { experiment: "17.9.", actual: 0, label: '0%' }
-            ]}
+            data={showChartHum}
             x="experiment"
             y="actual"
           />
